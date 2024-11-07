164a2362401336dbe41ff15029e3764930478c7b
zwingthomas <zwingthomas@gmail.com>
2024-11-06 20:09:17 -0600

    Automate adding credentials

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -7,6 +7,10 @@ resource "aws_s3_bucket" "terraform_state" {
     Name        = "Terraform state bucket"
     Environment = "Jenkins"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_versioning" "x" {
