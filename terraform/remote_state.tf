2b27e1a6bdb08f4d0b79ad0907ac422664a0fd83
zwingthomas <zwingthomas@gmail.com>
2024-11-13 20:23:10 -0600

    Working on threading plugins

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -1,25 +1,25 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tfstate-354923279633"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "tfstate-354923279633"
 
-  provider = aws.us_east
+#   provider = aws.us_east
 
-  tags = {
-    Name        = "Terraform state bucket"
-    Environment = "Jenkins"
-  }
+#   tags = {
+#     Name        = "Terraform state bucket"
+#     Environment = "Jenkins"
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "x" {
-  bucket = aws_s3_bucket.terraform_state.id
+# resource "aws_s3_bucket_versioning" "x" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = "terraform-lock-table"
